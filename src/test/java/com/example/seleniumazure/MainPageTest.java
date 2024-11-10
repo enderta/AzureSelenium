4bad069f39732e62964372b39cd4857104081bef
enderta <endertanver@gmail.com>
2024-11-10 20:36:20 +0000

    first

--- src/test/java/com/example/seleniumazure/MainPageTest.java
+++ src/test/java/com/example/seleniumazure/MainPageTest.java
@@ -18,16 +18,21 @@ public class MainPageTest {
 
     @BeforeEach
     public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://www.jetbrains.com/");
-driver.findElement(By.xpath("//button[@data-jetbrains-cookies-banner-action='ACCEPT_ALL']")).click();
-        mainPage = new MainPage(driver);
-    }
+
+            ChromeOptions options = new ChromeOptions();
+            // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
+            options.addArguments("--remote-allow-origins=*");
+            options.addArguments("--headless"); // Add this line to run Chrome in headless mode
+            options.addArguments("--no-sandbox"); // Add this line to disable the sandbox
+            options.addArguments("--disable-dev-shm-usage"); // Add this line to disable /dev/shm usage
+            driver = new ChromeDriver(options);
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver.get("https://www.jetbrains.com/");
+            driver.findElement(By.xpath("//button[@data-jetbrains-cookies-banner-action='ACCEPT_ALL']")).click();
+            mainPage = new MainPage(driver);
+        }
+
 
     @AfterEach
     public void tearDown() {
